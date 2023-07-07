3f292f19a1433a75a8c8fd3464cf81483c3c250c
Sota Giorgadze <giorgadze@inbox.lv>
2023-07-07 13:41:45 +0300

    * update next

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: [
-      "avatars.githubusercontent.com", 
+      "avatars.githubusercontent.com",
       "lh3.googleusercontent.com",
-      "res.cloudinary.com"
+      "res.cloudinary.com",
     ],
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
